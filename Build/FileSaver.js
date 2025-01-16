700735f36ea22e31f904f1308944ac2d6cbc2c34
olyislam <116255813+oly9@users.noreply.github.com>
2025-01-16 15:04:43 +0600

    new build

--- Build/FileSaver.js
+++
@@ -1,9 +0,0 @@
-function SaveFile(filename, content) {
-  var blob = new Blob([content], { type: 'text/plain' });
-  var link = document.createElement('a');
-  link.href = URL.createObjectURL(blob);
-  link.download = filename;
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
-}
